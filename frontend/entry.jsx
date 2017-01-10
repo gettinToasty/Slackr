0047a3f7acde490ef52544912fc200bc6aa2b040
gettinToasty <sean.r.beyer@gmail.com>
2017-01-10 10:22:33 -0800

    Set up React and entry point

---
+++ frontend/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>React Works</h1>,
+    document.getElementById('root')
+  );
+});
