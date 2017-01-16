be8d50af188623052237cb1745c5c420572bddd9
gettinToasty <sean.r.beyer@gmail.com>
2017-01-16 15:11:27 -0800

    Add dynamic search to ChannelList modal

--- frontend/components/chat_window/sidebar/channel_list.jsx
+++ frontend/components/chat_window/sidebar/channel_list.jsx
@@ -4,17 +4,32 @@ import ChannelListItem from './channel_list_item';
 class ChannelList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { query: "" };
+    this.state = { query: "", channels: this.props.channels };
+    this.updateQuery = this.updateQuery.bind(this);
+  }
+
+  updateQuery(e) {
+    this.setState({ query: e.target.value }, () => {
+      let channels = this.props.channels.filter(channel => (
+        channel.title.includes(this.state.query)
+      ));
+      if(this.state.query === "") { channels = this.props.channels; }
+      this.setState({ channels });
+    });
   }
 
   render() {
     return (
       <div>
         <h2>Browse All {this.props.channels.length} Channels</h2>
-        <input type='text' />
+        <input
+          type='text'
+          onChange={this.updateQuery}
+          value={this.state.query}
+          placeholder='Search Channels' />
         <ul>
           {
-            this.props.channels.map(channel => (
+            this.state.channels.map(channel => (
               <ChannelListItem
                 channel={channel}
                 key={channel.id}
