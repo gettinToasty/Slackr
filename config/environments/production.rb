f84887138ec4441b878b0c9487233dfe6401a6f4
gettinToasty <sean.r.beyer@gmail.com>
2017-01-16 17:40:15 -0800

    Set custom websocket url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://slackrapp.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://slackrapp.herokuapp.com', /https:\/\/herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
